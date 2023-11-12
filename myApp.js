fc77806b8c36f2af4b43a081a8901bbac453603a
Akthab <mhmdakthab6@gmail.com>
2023-11-12 22:56:21 +0530

    feat(): implemented middleware

--- myApp.js
+++ myApp.js
@@ -4,10 +4,21 @@ require('dotenv').config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/*', function (req, res, next) {
-	console.log(req.method + ' ' + req.path + ' - ' + req.ip);
-	next();
-});
+// app.use('/*', function (req, res, next) {
+// 	console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+// 	next();
+// });
+
+app.get(
+	'/now',
+	function (req, res, next) {
+		req.time = new Date().toString();
+		next();
+	},
+	function (req, res) {
+		res.json({ time: req.time });
+	}
+);
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
