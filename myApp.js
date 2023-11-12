348600252e892048fe46d9924d1699783fc6397e
Akthab <mhmdakthab6@gmail.com>
2023-11-12 16:00:26 +0530

    feat(): updated package.json

--- myApp.js
+++ myApp.js
@@ -6,4 +6,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/json', function (req, res) {
+	const data = { message: 'Hello json' };
+	res.send(data);
+});
 module.exports = app;
