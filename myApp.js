1ec2b26133f6d0a9858db3bb671358e48edfcbb4
Akthab <mhmdakthab6@gmail.com>
2023-11-12 23:24:42 +0530

    feat(): query params

--- myApp.js
+++ myApp.js
@@ -24,6 +24,10 @@ app.get('/:word/echo', function (req, res) {
 	res.json({ echo: req.params.word });
 });
 
+app.get('/name', function (req, res) {
+	res.json({ name: req.query.first + ' ' + req.query.last });
+});
+
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
